c4f1fcd57e584864ec175f15585681795c7ba153
mingo <domingo.em@gmail.com>
2019-05-07 23:33:36 +0200

    move mm

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,8 @@
 public class MarsRover {
     public String execute(String command) {
+        if (command.equals("MM")){
+            return "2:0:N";
+        }
         return command.equals("M") ? "1:0:N" : command.equals("R") ? "0:0:E" : "0:0:W";
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,29 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class MarsRoverTest {
 
+
+    private MarsRover marsRovers;
+
+    @BeforeEach
+    void setUp() {
+        marsRovers = new MarsRover();
+    }
+
     @Test
     public void given_initial_position_when_L_then_turn_left_rovers() {
-        MarsRover marsRovers = new MarsRover();
         String position = marsRovers.execute("L");
         assertThat(position, is("0:0:W"));
     }
 
     @Test
     public void given_initial_position_when_R_turn_right_rovers() {
-        MarsRover marsRovers = new MarsRover();
         String position = marsRovers.execute("R");
         assertThat(position, is("0:0:E"));
     }
 
-    @Test
-    public void given_initial_position_when_M_move_foward_rovers() {
-        MarsRover marsRovers = new MarsRover();
-        String position = marsRovers.execute("M");
-        assertThat(position, is("1:0:N"));
+    @ParameterizedTest
+    @CsvSource({"M, 1:0:N",
+                 "MM, 2:0:N",})
+    public void given_initial_position_when_M_move_foward_rovers(String command, String expected) {
+        String position = marsRovers.execute(command);
+        assertThat(position, is(expected));
     }
 
 }
