632b4441e59a64d08e98b153c699ffd9cc50863a
mingo <domingo.em@gmail.com>
2019-05-07 23:14:00 +0200

    rename by order of cristina

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -6,21 +6,21 @@ import static org.hamcrest.Matchers.is;
 public class MarsRoverTest {
 
     @Test
-    public void turn_left_rovers_when_l() {
+    public void given_initial_position_when_L_then_turn_left_rovers() {
         MarsRover marsRovers = new MarsRover();
         String position = marsRovers.execute("L");
         assertThat(position, is("0:0:W"));
     }
 
     @Test
-    public void turn_right_rovers_when_r() {
+    public void given_initial_position_when_R_turn_right_rovers() {
         MarsRover marsRovers = new MarsRover();
         String position = marsRovers.execute("R");
         assertThat(position, is("0:0:E"));
     }
 
     @Test
-    public void move_foward_rovers_when_m() {
+    public void given_initial_position_when_M_move_foward_rovers() {
         MarsRover marsRovers = new MarsRover();
         String position = marsRovers.execute("M");
         assertThat(position, is("1:0:N"));
