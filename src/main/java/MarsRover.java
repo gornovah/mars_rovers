32785a9f108343d87a4235c0a6dd6ebcab41f0f0
mingo <domingo.em@gmail.com>
2019-05-07 22:48:22 +0200

    move to left and right

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,5 @@
 public class MarsRover {
+    public String execute(String command) {
+        return command.equals("R") ? "0:0:E" : "0:0:W";
+    }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class MarsRoverTest {
+
+    @Test
+    public void turn_left_rovers_when_l() {
+        MarsRover marsRovers = new MarsRover();
+        String position = marsRovers.execute("L");
+        assertThat(position, is("0:0:W"));
+    }
+
+    @Test
+    public void turn_right_rovers_when_r() {
+        MarsRover marsRovers = new MarsRover();
+        String position = marsRovers.execute("R");
+        assertThat(position, is("0:0:E"));
+    }
+
 }
